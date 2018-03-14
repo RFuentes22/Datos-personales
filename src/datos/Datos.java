5ccc9131b74a606bd69808757886074ce02295c6
Tito <Tito@Tito-PC>
2018-03-14 12:52:53 -0600

    primera version

---
+++ src/datos/Datos.java
@@ -0,0 +1,16 @@
+package datos;
+
+//autor: Roberto Fuentes 00062217
+
+public class Datos {
+
+    public static void main(String[] args) {
+        String nombre = "Roberto Antonio", apellido = "Fuentes Guardado", direccion = "San Salvador", estado_civil = "Soltero", ocupacion = "Estudiante", correo = "00062217@uca.edu.sv", numcarnet = "00062217", fechanac = "22-07-1998";
+        int numtel = 24131352;
+        // TODO code application logic here
+        System.out.println("Nombres: " + nombre + " \nApellidos: " + apellido + "\nDireccion: " + direccion
+                + "\tEstado Civil: " + estado_civil + "\nOcupacion: " + ocupacion + "           Correo electronico: " + correo
+                + "\nTelefono: " + numtel + "              Carnet: " + numcarnet + "\nFecha de nacimiento: " + fechanac);
+    }
+
+}
